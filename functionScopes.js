b19f8c5bf20c95a74482c215ceef298d98bb42db
nitin reddy <reddynitin3@gmail.com>
2020-12-23 15:19:22 +0530

    Added the example for function scope

--- functionScopes.js
+++ functionScopes.js
@@ -2,8 +2,8 @@ let x = 10;
 if (true) {
   let y = 20;
   var z = 30;
-  console.log(x + y + z)
+  console.log(x + y + z);
 }
 
-console.log(y + z) // This results in reference error for y
-console.log(x + z) //40
+console.log(y + z); // This results in reference error for y
+console.log(x + z); //40
